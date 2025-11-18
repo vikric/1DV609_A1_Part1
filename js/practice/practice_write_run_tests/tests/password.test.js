dbb24ea81e50c87fa1446da5734f387286ba3f8f
VikRic <vr222im@student.lnu.se>
2025-11-18 14:51:57 +0100

    All run tests updated

--- js/practice/practice_write_run_tests/tests/password.test.js
+++ js/practice/practice_write_run_tests/tests/password.test.js
@@ -1,46 +1,50 @@
 // Select one of the Password versions to test
 
-/* import { Password } from "../src/BugDoesNotHash"; */
-
-// import { Password } from '../src/BugisPasswordAlwaysSame'
-// import { Password } from '../src/BugMissingNumberCheck'
-// import { Password } from '../src/BugMissingPasswordCheck'
-/* import { Password } from "../src/BugNeverContainsNumbers"; */
-
-// import { Password } from '../src/BugWrongHashingAlgorithm'
-// import { Password } from '../src/BugWrongMessage'
-/* import { Password } from "../src/Correct"; */
+import { Password } from "../src/Correct";
 
 // Correct!
+/* import { Password } from '../src/BugMissingPasswordCheck' */
 /* import { Password } from "../src/BugToShortPassword"; */
 /* import { Password } from "../src/BugVeryShort"; */
 /* import { Password } from '../src/BugDoesNotTrim' */
+/* import { Password } from '../src/BugWrongMessage' */
+/* import { Password } from '../src/BugMissingNumberCheck' */
+/* import { Password } from "../src/BugDoesNotHash"; */
+/* import { Password } from '../src/BugisPasswordAlwaysSame' */
+/* import { Password } from "../src/BugWrongHashingAlgorithm"; */
+/* import { Password } from "../src/BugNeverContainsNumbers"; */
 
 describe("Password class, test suite", () => {
   //put constants here to increase readability
   const correctPassword = "MyPassword1234";
-  /* const password = new Password(correctPassword); */
+
+  const incorrectPasswords = [
+    "", // Catches bugs on empty passwords
+    "12345", // Catches bugs on passwords with length less than 6
+    "abcdef12345", // Catches bugs on passwords with length less than 11
+    "  12345678901  ", // Catches bugs on passwords that does not trim
+  ];
+
+  test("Check if password argument is instanceOf Password", () => {
+    expect(() => {
+      new Password(correctPassword).isPasswordSame("Test");
+    }).toThrow("Invalid argument");
+  });
 
   test("constructor throws for all too short passwords", () => {
-    const incorrectPasswords = [
-      "12345",              // Catches bugs on passwords with length less than 6
-      "abcdef12345",        // Catches bugs on passwords with length less than 11
-      "   12345678901 ",    // Catches bugs on passwords that does not trim
-    ];
-
     for (const pw of incorrectPasswords) {
       expect(() => new Password(pw)).toThrow("Too short password");
     }
   });
 
-  test("throws error if password does not contain numbers", () => {
+  test("constructor throws for all passwords not contain numbers", () => {
     expect(() => {
       new Password("TestTestTestTest");
     }).toThrow("No number found");
   });
 
   test("Return the hashed password", () => {
-    const password = new Password(correctPassword)
+    const password = new Password(correctPassword);
     let hashValue = 7;
     for (let i = 0; i < correctPassword.length; i++) {
       hashValue = hashValue * 31 + correctPassword.charCodeAt(i);
@@ -50,14 +54,8 @@ describe("Password class, test suite", () => {
 
   test("Check if password is the same", () => {
     const anotherCorrectPassword = new Password("NewPassWord1234");
-    const password = new Password(correctPassword)
+    const password = new Password(correctPassword);
     const actual = password.isPasswordSame(anotherCorrectPassword);
     expect(actual).toBeFalsy();
-  });
-
-  test("Check if password argument is instanceOf Password", () => {
-    expect(() => {
-      new Password(correctPassword).isPasswordSame("Test");
-    }).toThrow("Invalid argument");
   });
 });
