0b55a4c5b36aafb0c0fac57bf88cac6f6d610adb
VikRic <vr222im@student.lnu.se>
2025-11-19 13:51:50 +0100

    Update tests

--- js/practice/practice_write_run_tests/tests/password.test.js
+++ js/practice/practice_write_run_tests/tests/password.test.js
@@ -1,18 +1,22 @@
 // Select one of the Password versions to test
 
-import { Password } from "../src/Correct";
+/* import { Password } from "../src/Correct"; */
+
 /* import { Password } from "../src/MyBug"; */
+import { Password } from "../src/BugDoesNotHash";
+
+/* import { Password } from '../src/BugWrongMessage' */
 
 // Correct!
 /* import { Password } from '../src/BugMissingPasswordCheck' */
 /* import { Password } from "../src/BugToShortPassword"; */
 /* import { Password } from "../src/BugVeryShort"; */
-/* import { Password } from '../src/BugDoesNotTrim' */
-/* import { Password } from '../src/BugWrongMessage' */
-/* import { Password } from '../src/BugMissingNumberCheck' */
-/* import { Password } from "../src/BugDoesNotHash"; */
-/* import { Password } from '../src/BugisPasswordAlwaysSame' */
+/*  import { Password } from '../src/BugDoesNotTrim' */
 /* import { Password } from "../src/BugWrongHashingAlgorithm"; */
+/* import { Password } from '../src/BugisPasswordAlwaysSame' */
+
+/* import { Password } from '../src/BugMissingNumberCheck' */
+
 /* import { Password } from "../src/BugNeverContainsNumbers"; */
 
 describe("Password class, test suite", () => {
@@ -22,13 +26,12 @@ describe("Password class, test suite", () => {
   const incorrectPasswords = [
     "", // Catches bugs on empty passwords
     "12345", // Catches bugs on passwords with length less than 6
-    "abcde12345", // Catches bugs on passwords with length less than 11
-    "  12345678901  ", // Catches bugs on passwords that does not trim or is less than 12.
+    "abcde123456", // Catches bugs on passwords with length less than 11
   ];
 
-  test("constructor should throw exception if password argument is not instanceOf Password", () => {
+  test("should throw exception if argument is not instanceOf Password", () => {
     expect(() => {
-      new Password(correctPassword).isPasswordSame("Test");
+      new Password(correctPassword).isPasswordSame("password");
     }).toThrow("Invalid argument");
   });
 
@@ -36,6 +39,14 @@ describe("Password class, test suite", () => {
     for (const pw of incorrectPasswords) {
       expect(() => new Password(pw)).toThrow("Too short password");
     }
+  });
+
+  test("constructor should trim passwords", () => {
+    const passwordWithSpaces = new Password("  1234567890123  ");
+    const passwordWithOutSpaces = new Password("1234567890123");
+    expect(passwordWithSpaces.getPasswordHash()).toBe(
+      passwordWithOutSpaces.getPasswordHash()
+    );
   });
 
   test("constructor should throw exception for passwords without numbers", () => {
--- table.md
+++ table.md
@@ -1,9 +1,10 @@
 | Version       | Correct | NotHash | NoTrim |  PassAlwaysSame | MissingNumberCheck | MissingPasswordCheck | ContainsNumber | ShortPassword | VeryShort | WrongHash | Wrong Message |
 | --- | ---| --- | --- | --- | --- | --- | ---| --- | --- | --- | --- | --- | ---| --- | --- | --- | --- |
-| InstanceOf Password argument  | ✅ | ❌ | ✅ | ✅ | ✅ | ✅ | ❌ | ✅ | ✅ | ✅ | ✅ | ❌ | ✅ | ✅ | ✅ |
+| InstanceOf Password argument  | ✅ | ✅ | ✅ | ✅ | ✅ | ✅ | ❌ | ✅ | ✅ | ✅ | ✅ | ❌ | ✅ | ✅ | ✅ |
+| Same password check           | ✅ | ✅ | ❌ | ❌ | ✅ | ✅ | ❌ | ✅ | ✅ | ✅ | ✅ | ❌ | ✅ | ✅ | ✅ |
 | Short passwords               | ✅ | ✅ | ❌ | ✅ | ✅ | ✅ | ❌ | ✅ | ✅ | ✅ | ✅ | ❌ | ✅ | ✅ | ✅ |
 | Passwords without numbers     | ✅ | ✅ | ❌ | ❌ | ✅ | ✅ | ❌ | ✅ | ✅ | ✅ | ✅ | ❌ | ✅ | ✅ | ✅ |
-| Passwords with numbers        | ✅ | ❌ | ✅ | ✅ | ✅ | ✅ | ❌ | ✅ | ✅ | ✅ | ✅ | ❌ | ✅ | ✅ | ✅ |
-| Password hashing              | ✅ | ✅ | ❌ | ✅ | ✅ | ✅ | ❌ | ✅ | ✅ | ✅ | ✅ | ❌ | ✅ | ✅ | ✅ |
+| Passwords with numbers        | ✅ | ✅ | ✅ | ✅ | ✅ | ✅ | ❌ | ✅ | ✅ | ✅ | ✅ | ❌ | ✅ | ✅ | ✅ |
+| Password hashing              | ✅ | ❌ | ❌ | ✅ | ✅ | ✅ | ❌ | ✅ | ✅ | ✅ | ✅ | ❌ | ✅ | ✅ | ✅ |
 | Same password check           | ✅ | ✅ | ❌ | ❌ | ✅ | ✅ | ❌ | ✅ | ✅ | ✅ | ✅ | ❌ | ✅ | ✅ | ✅ |
-| Coverage                      | 100% | 100% | 100% | 100% | 100% | 100% | 100% | 100% | 100% | 100% | 100% | 100% | 100% | 100% | 100% |
+| Coverage                      | 100% | 100% | 100% | 100% | 93% | 93% | 100% | 100% | 100% | 100% | 100% | 100% | 100% | 100% | 100% |
